2ddd31eae5f1a7d5d06433f10a4a37840674bc6a
Mauro dos Santos <maurosantosarruda@gmail.com>
2018-11-11 13:36:26 -0300

    initial version

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
+
+const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
+
+const port = process.env.PORT || 3000
+
+app.get('/', async(request, response) => {
+    response.render('index')
+})
+
+app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log ('erro')
+    }else{
+        console.log ('servidor está rodando na porta:', port)
+    }
+})
